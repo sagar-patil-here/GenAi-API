5249e51cf619d3fa3ab1d843f9a188de3f7afb3f
Sagar Patil <sagarpatil22112004@gmail.com>
2025-02-17 12:17:57 +0530

    edit at server.js port

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.post("/api/chat", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(8080, "0.0.0.0", () => {
+  console.log("Server running on port 8080");
 });
+
