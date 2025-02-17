ede882028fc45f4494c7217976565ee835a61c72
Sagar Patil <sagarpatil22112004@gmail.com>
2025-02-17 23:01:58 +0530

    changed frontend for respponse

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
